b75e0e67aa270cc7108a571c020ab648daf8932b
joeyhanni2 <joeyhanni@gmail.com>
2024-10-18 14:59:19 +0000

    4-tasks.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python3
-"""This module contains an asynchronous coroutine
-that waits for a random delay and returns it.
-"""
-
+'''Task 0's module.
+'''
 import asyncio
 import random
-from typing import Union
 
 
 async def wait_random(max_delay: int = 10) -> float:
-
-delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-async def main() -> None:
-
-"""Calls wait_random coroutine and prints the result."""
-    random_delay: float = await wait_random(5)
-    print(f"Waited for {random_delay:.2f} seconds")
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""This module provides a function to create an asyncio.Task
-for the wait_random coroutine.
-"""
-
+'''Task 3's module.
+'''
 import asyncio
-from 0_basic_async_syntax import wait_random
 
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-
-
-    return asyncio.create_task(wait_random(max_delay))
+    '''Create
+    '''
+    return asyncio.create_task(wait_random(max_delay)
